95c5b21cb1313690fa1d334a39b7afaf39724f62
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2025-01-23 21:13:46 -0500

    This could use some work, probably needs to be split into multiple PRs

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -5,13 +5,6 @@ MAP=$1
 
 echo Testing $MAP
 
-tools/deploy.sh ci_test
-mkdir ci_test/config
-mkdir ci_test/data
-
-#test config
-cp tools/ci/ci_config.txt ci_test/config/config.txt
-
 #set the map
 cp _maps/$MAP.json ci_test/data/next_map.json
 
