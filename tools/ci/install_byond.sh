12a89264425f38e561221c6546ade6cc4af9cbe7
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2025-02-04 19:17:33 -0500

    Make Windows cache its BYOND installs; plus, use `actions/setup-node` instead of manually installing Node

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -10,15 +10,50 @@ if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $
 then
   echo "Using cached directory."
 else
-  echo "Setting up BYOND."
+  echo "Setting up BYOND version $BYOND_MAJOR.$BYOND_MINOR"
   rm -rf "$HOME/BYOND"
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
-  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+
+  # Download BYOND for this platform.
+  case "$RUNNER_OS" in
+    "Linux")
+      echo "...for Linux."
+      curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+      ;;
+
+    "Windows")
+      echo "...for Windows."
+      curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond.zip" -o byond.zip
+      ;;
+
+    *)
+      echo "The OS for this runner does not have a script to download BYOND. If BYOND is available for this OS, please fix this!"
+      exit 1
+      ;;
+  esac
+
   unzip byond.zip
   rm byond.zip
-  cd byond
-  make here
+
+  # Perform any extra steps needed to set up BYOND on this platform.
+  case "$RUNNER_OS" in
+    "Linux")
+      cd byond
+      make here
+      cd ..
+      ;;
+
+    "Windows")
+      echo "DM_EXE=$HOME/BYOND/byond/bin/dm.exe" >> $GITHUB_ENV
+      ;;
+
+    *)
+      echo "The OS for this runner does not have a script to install BYOND. If BYOND is available for this OS, please fix this!"
+      exit 1
+      ;;
+  esac
+
   echo "$BYOND_MAJOR.$BYOND_MINOR" > "$HOME/BYOND/version.txt"
   cd ~/
 fi
